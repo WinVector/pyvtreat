327d6d12b94336fc617a287ddaa7f0026d0da843
John Mount <jmount@win-vector.com>
2019-07-20 11:20:53 -0700

    pick argument pattern for k_way_cross_plan()

--- vtreat/__init__.py
+++ vtreat/__init__.py
@@ -2,16 +2,27 @@
 
 
 import numpy
-import pandas
 
 
 
-def k_way_cross_plan(n, k):
-    """randomly split range(n) into k disjoint groups"""
-    grp = [i % k for i in range(n)]
+def k_way_cross_plan(n_rows, 
+                     k_folds, 
+                     *, 
+                     data_frame=None, 
+                     y=None):
+    """randomly split range(n_rows) into k_folds disjoint groups"""
+    if n_rows<2:
+        raise Exception('n_rows should be at least 2')
+    if k_folds>=n_rows:
+        k_folds = n_rows-1
+    # first assign groups modulo k (ensuring at least one in each group)
+    grp = [i % k_folds for i in range(n_rows)]
+    # now shuffle
     numpy.random.shuffle(grp)
-    plan = [ { "train"  : [i for i in range(n) if grp[i] != j],
-               "test" : [i for i in range(n) if grp[i] == j] } for j in range(k) ]
+    plan = [ 
+            { "train"  : [i for i in range(n_rows) if grp[i] != j],
+               "test" : [i for i in range(n_rows) if grp[i] == j] } for j in range(k_folds) 
+            ]
     return(plan)
 
 
