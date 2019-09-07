a41958250101c7054ea04de2b251d28531802f8b
John Mount <jmount@win-vector.com>
2019-09-07 07:25:13 -0700

    clean up, bump version

--- pkg/tests/test_multinomial.py
+++ pkg/tests/test_multinomial.py
@@ -3,32 +3,44 @@ import numpy.random
 import pandas
 import vtreat
 
+
 def test_multinomial():
     numpy.random.seed(2019)
 
     n_rows = 1000
-    y_levels = ['a', 'b', 'c']
-    d = pandas.DataFrame({'y': numpy.random.choice(y_levels, size=n_rows)})
+    y_levels = ["a", "b", "c"]
+    d = pandas.DataFrame({"y": numpy.random.choice(y_levels, size=n_rows)})
     # signal variables, correlated with y-levels
     for i in range(2):
         vmap = {yl: numpy.random.normal(size=1)[0] for yl in y_levels}
-        d['var_n_' + str(i)] = [vmap[li] + numpy.random.normal(size=1)[0] for li in d['y']]
+        d["var_n_" + str(i)] = [
+            vmap[li] + numpy.random.normal(size=1)[0] for li in d["y"]
+        ]
     for i in range(2):
         col = numpy.random.choice(y_levels, size=n_rows)
-        col = [col[i] if numpy.random.uniform(size=1)[0]<=0.8 else d['y'][i] for i in range(n_rows)]
-        d['var_c_' + str(i)] = col
+        col = [
+            col[i] if numpy.random.uniform(size=1)[0] <= 0.8 else d["y"][i]
+            for i in range(n_rows)
+        ]
+        d["var_c_" + str(i)] = col
     # noise variables, uncorrelated with y-levels
     for i in range(2):
-        d['noise_n_' + str(i)] = [numpy.random.normal(size=1)[0] + numpy.random.normal(size=1)[0] for li in d['y']]
+        d["noise_n_" + str(i)] = [
+            numpy.random.normal(size=1)[0] + numpy.random.normal(size=1)[0]
+            for li in d["y"]
+        ]
     for i in range(2):
-        d['noise_c_' + str(i)] = numpy.random.choice(y_levels, size=n_rows)
+        d["noise_c_" + str(i)] = numpy.random.choice(y_levels, size=n_rows)
 
-
-    treatment = vtreat.MultinomialOutcomeTreatment(
-        outcome_name='y')
-    cross_frame = treatment.fit_transform(d, d['y'])
+    treatment = vtreat.MultinomialOutcomeTreatment(outcome_name="y")
+    cross_frame = treatment.fit_transform(d, d["y"])
     sf = treatment.score_frame_
 
-    targets = sf.loc[numpy.logical_and([v in ['var_n_0', 'var_n_1'] for v in sf.variable],
-                                        sf.treatment=='clean_copy'), :]
+    targets = sf.loc[
+        numpy.logical_and(
+            [v in ["var_n_0", "var_n_1"] for v in sf.variable],
+            sf.treatment == "clean_copy",
+        ),
+        :,
+    ]
     assert all(targets.recommended)
--- pkg/tests/test_unsupervised.py
+++ pkg/tests/test_unsupervised.py
@@ -1,18 +1,18 @@
 import numpy.random
 import pandas
-import vtreat # https://github.com/WinVector/pyvtreat
+import vtreat  # https://github.com/WinVector/pyvtreat
+
 
 def test_unsupervised():
     numpy.random.seed(235)
-    zip = ['z' + str(i + 1).zfill(5) for i in range(15)]
-    d = pandas.DataFrame({'zip': numpy.random.choice(zip, size=1000)})
+    zip = ["z" + str(i + 1).zfill(5) for i in range(15)]
+    d = pandas.DataFrame({"zip": numpy.random.choice(zip, size=1000)})
     d["const"] = 1
     d["const2"] = "b"
 
     transform = vtreat.UnsupervisedTreatment(
-        params=vtreat.unsupervised_parameters({
-            'indicator_min_fraction': 0.01,
-        }))
+        params=vtreat.unsupervised_parameters({"indicator_min_fraction": 0.01})
+    )
 
     d_treated = transform.fit_transform(d)
 
