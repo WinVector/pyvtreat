f189a78dd74f94bbf6424037a82de58ca43cf275
John Mount <jmount@win-vector.com>
2019-11-10 08:15:20 -0800

    adjust python versions

--- pkg/setup.py
+++ pkg/setup.py
@@ -74,6 +74,7 @@ setuptools.setup(
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'License :: OSI Approved :: BSD License',
     ],
     long_description=LONG_DESCRIPTION
