6eb2bd025708b427cd3cbb3376e46b0834194f38
John Mount <jmount@win-vector.com>
2019-11-21 08:58:45 -0800

    test de-duping

--- pkg/tests/test_col_name_issues.py
+++ pkg/tests/test_col_name_issues.py
@@ -16,7 +16,7 @@ def test_col_dups_1():
         transform.fit_transform(d, d["y"])
 
 
-def test_xgboost_col_name_issue():
+def test_xgboost_col_name_issue_1():
     # https://stackoverflow.com/questions/48645846/pythons-xgoost-valueerrorfeature-names-may-not-contain-or
     # ValueError('feature_names may not contain [, ] or <')
     d = pandas.DataFrame({'x': ['[', ']', '<' , '>']})
@@ -28,3 +28,19 @@ def test_xgboost_col_name_issue():
     cols = d_transformed.columns
     for col in cols:
         assert not any(c in col for c in "[]<>")
+    assert len(set(cols)) == len(cols)
+
+
+def test_xgboost_col_name_issue_2():
+    # https://stackoverflow.com/questions/48645846/pythons-xgoost-valueerrorfeature-names-may-not-contain-or
+    # ValueError('feature_names may not contain [, ] or <')
+    d = pandas.DataFrame({'x': ['[', ']', '<' , '_lt_']})
+
+    transform = vtreat.UnsupervisedTreatment(
+        var_list=['x']
+    )
+    d_transformed = transform.fit_transform(d, None)
+    cols = d_transformed.columns
+    for col in cols:
+        assert not any(c in col for c in "[]<>")
+    assert len(set(cols)) == len(cols)
