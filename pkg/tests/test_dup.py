06f4dd5997ad4aeebffa12148c11cb716ed0c861
John Mount <jmount@win-vector.com>
2020-06-24 14:17:28 -0700

    test forced error

---
+++ pkg/tests/test_dup.py
@@ -0,0 +1,53 @@
+
+import pytest
+
+import pandas
+import numpy
+import numpy.random
+import vtreat
+import vtreat.util
+
+
+def test_dup():
+    numpy.random.seed(46546)
+
+    def make_data(nrows):
+        d = pandas.DataFrame({"x": [0.1 * i for i in range(nrows)]})
+        d["y"] = d["x"] + numpy.sin(d["x"]) + 0.1 * numpy.random.normal(size=d.shape[0])
+        d["xc"] = ["level_" + str(5 * numpy.round(yi / 5, 1)) for yi in d["y"]]
+        d["x2"] = numpy.random.normal(size=d.shape[0])
+        d.loc[d["xc"] == "level_-1.0", "xc"] = numpy.nan  # introduce a nan level
+        d["yc"] = d["y"] > 0.5
+        return d
+
+    d = make_data(500)
+
+    transform = vtreat.BinomialOutcomeTreatment(
+        outcome_name="yc",  # outcome variable
+        outcome_target=True,  # outcome of interest
+        cols_to_copy=["y"],  # columns to "carry along" but not treat as input variables
+    )
+
+    d_prepared = transform.fit_transform(d, d["yc"])
+
+    with pytest.warns(UserWarning):
+        d_prepared_wrong = transform.transform(d)
+
+    transform = vtreat.BinomialOutcomeTreatment(
+        outcome_name="yc",  # outcome variable
+        outcome_target=True,  # outcome of interest
+        cols_to_copy=["y"],  # columns to "carry along" but not treat as input variables
+        params={'error_on_duplicate_frames': True}
+    )
+
+    d_prepared = transform.fit_transform(d, d["yc"])
+
+    with pytest.raises(ValueError):
+        d_prepared_wrong = transform.transform(d)
+
+    dtest = make_data(450)
+
+    dtest_prepared = transform.transform(dtest)
+
+
+
