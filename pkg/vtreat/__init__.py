dc9a78bc45b5f8c672b72039e1adb17199099d2b
John Mount <jmount@win-vector.com>
2020-06-25 09:45:45 -0700

    bump version while we work on a pipeline adapter strategy

--- pkg/vtreat/__init__.py
+++ pkg/vtreat/__init__.py
@@ -7,7 +7,7 @@ import numpy
 from vtreat.vtreat_api import *
 
 __docformat__ = "restructuredtext"
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 
 __doc__ = """
 This<https://github.com/WinVector/pyvtreat> is the Python version of the vtreat data preparation system
