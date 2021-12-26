eb71be1b43e9310cdf77f89c2ae2d26be3a59d4b
John Mount <jmount@win-vector.com>
2021-12-26 12:05:56 -0800

    bump version

--- pkg/build/lib/vtreat/__init__.py
+++ pkg/build/lib/vtreat/__init__.py
@@ -11,7 +11,7 @@ import numpy
 from vtreat.vtreat_api import *
 
 __docformat__ = "restructuredtext"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 __doc__ = """
 This<https://github.com/WinVector/pyvtreat> is the Python version of the vtreat data preparation system
