9c57049134e72d7b63058e11fbe2893bd1c3201a
John Mount <jmount@win-vector.com>
2023-09-27 16:05:42 -0600

    list packages on rebuild

--- rebuild.bash
+++ rebuild.bash
@@ -6,6 +6,7 @@ python3 setup.py sdist bdist_wheel
 # pip install dist/vtreat-*.tar.gz
 popd
 pip install --no-deps -e "$(pwd)/pkg"  # sym link to source files
+conda list --export > vtreat_dev_env_package_list.txt
 pdoc -o docs pkg/vtreat
 pytest --cov pkg/vtreat pkg/tests > coverage.txt
 # pytest --cov-report term-missing --cov pkg/vtreat pkg/tests > coverage.txt
